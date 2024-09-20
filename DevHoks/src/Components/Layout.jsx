547000f868b8dc5914c69f414286328af0ed108f
Abdul hameed <abdulhameed000650@gmail.com>
2024-09-21 01:59:11 +0500

    loader added

--- DevHoks/src/Components/Layout.jsx
+++ DevHoks/src/Components/Layout.jsx
@@ -1,22 +1,36 @@
 /* eslint-disable no-unused-vars */
-import Footer from './Layout/Footer/footer';
-import Navbar from './Layout/Header/navbar';
-import React from 'react';
-import PropTypes from 'prop-types';
+import Footer from "./Layout/Footer/footer";
+import Navbar from "./Layout/Header/navbar";
+import React from "react";
+import PropTypes from "prop-types";
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            {/* anything to display on top of the navbar will be here */}
-            <Navbar />
-            {children}
-            <Footer />
-        </>
-    );
+  return (
+    <>
+      {/* anything to display on top of the navbar will be here */}
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
 };
 
 Layout.propTypes = {
-    children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 };
 
 export default Layout;
+
+{/* <BeatLoader
+color="#00a84e"
+    cssOverride={{
+      display: "block",
+      margin: "0 auto",
+      borderColor: "#00a84e",
+      zIndex: 9999,
+}}
+loading
+margin={0}
+size={48}
+speedMultiplier={1}
+/> */}
