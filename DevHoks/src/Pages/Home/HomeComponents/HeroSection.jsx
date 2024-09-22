56214d7dddff48407d0d25a9992f74169b0718f2
Abdul hameed <abdulhameed000650@gmail.com>
2024-09-22 16:40:41 +0500

    type writter added

--- DevHoks/src/Pages/Home/HomeComponents/HeroSection.jsx
+++ DevHoks/src/Pages/Home/HomeComponents/HeroSection.jsx
@@ -11,10 +11,10 @@ const HeroSection = () => {
     return (
       
       <div className="hero__section__wrapper">
-          <div className="relative isolate px-6 pt-14 lg:px-8">
+          <div className="relative isolate px-14 lg:px-8">
         <div
           aria-hidden="true"
-          className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+          className="absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
         >
           <div
             style={{
@@ -24,18 +24,18 @@ const HeroSection = () => {
             className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"
           />
         </div>
-        <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
+        <div className="mx-auto max-w-2xl py-20 sm:py-28 lg:py-36">
           <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-            <div className="relative rounded-full px-3 py-1 text-sm leading-6 text-gray-600 ring-1 ring-green-600 hover:ring-green-800">
+            <div className="relative rounded-full px-4 py-2 text-md leading-6 font-medium text-green-900 bg-green-300 ring-1 ring-green-300">
               Delivering The Solution Over The Globe.{' '}
               
             </div>
           </div>
           <div className="text-center">
             <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl">
-            We Tackle Every Nech {' '}<span><Typewriter
+            We Tackle Every Nech {' '}<span className="text__gradient"><Typewriter
             words={services}
-            loop={0}
+            loop={1}
             cursor
             cursorStyle='|'
             typeSpeed={70}
@@ -52,15 +52,17 @@ const HeroSection = () => {
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="rounded-md bg-gray-900 px-3.5 py-2.5 text-md font-semibold text-white shadow-sm hover:bg-green-800 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-gray-800"
               >
                 Contact Now
-              </a>
+                </a>
+                <span className="text-gray-500">OR</span>
               <a
                 href="#"
-                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Free Consultion
+                className="rounded-md ring-1 ring-green-700 px-3.5 py-2.5 text-md font-semibold text-green-700 hover:bg-green-800 hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-green-800"
+                >
+                  <i className="fa-solid fa-headset pr-2"></i>
+                 Free Consultion
               </a>
             </div>
           </div>
