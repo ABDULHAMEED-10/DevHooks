9061b07acda6fac140005d321d8282ff8bbbff2c
Abdul hameed <abdulhameed000650@gmail.com>
2024-11-27 15:09:29 +0500

    updates

--- DevHoks/src/Pages/Home/HomeComponents/CallToWork/Connect.jsx
+++ DevHoks/src/Pages/Home/HomeComponents/CallToWork/Connect.jsx
@@ -9,7 +9,7 @@ export const Connect = () => {
         </div>
         <div className="mt-3 flex flex-col items-center justify-between gap-3 md:flex-row">
           <div>
-            <p className="text-sm lg:text-md font-normal tracking-wide text-gray-100 max-w-3xl ">
+            <p className="text-sm lg:text-md font-normal tracking-wide text-white max-w-3xl ">
               We are always ready to help you with your business. Let&apos;s
               connect and make your business more profitable. Press the button
               contact and email us or call us to get started. We are 24/7
@@ -19,7 +19,7 @@ export const Connect = () => {
           <div className="">
             <a
               href="https://wa.me/+923000713825"
-              className="inline-block px-6 py-1.5 text-lg font-semibold text-gray-700 bg-gray-200 rounded-lg hover:bg-gray-100 hover:text-gray-900 transition duration-300"
+              className="inline-block px-6 py-1.5 text-lg font-semibold text-gray-800 bg-gray-200 rounded-lg hover:bg-gray-100 hover:text-gray-900 focus:bg-gray-100 focus:text-gray-900 transition duration-300"
             >
               Contact
             </a>
--- DevHoks/src/Pages/Home/HomeComponents/TrusedBy/Brand.jsx
+++ DevHoks/src/Pages/Home/HomeComponents/TrusedBy/Brand.jsx
@@ -3,13 +3,12 @@ import { brandsData } from "../../../../demoData";
 
 export default function Brand() {
   return (
-    <section className="bg-white py-10 dark:bg-dark overflow-hidden">
+    <section className="bg-white pb-10 dark:bg-dark overflow-hidden">
       <div className="container mx-auto">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
-
-          <p className="mt-6 text-3xl font-semibold text-gray-900 text-center flex justify-center items-center md:text-4xl lg:text-5xl">
-          Trusted by 6,600+ Clients and Companies including
+            <p className="mt-6 text-3xl font-semibold text-gray-900 text-center flex justify-center items-center md:text-4xl lg:text-5xl">
+              Trusted by 6,600+ Clients and Companies including
             </p>
             <div className="flex items-center justify-evenly mt-8 md:mt-10 md:mb-16">
               {brandsData.map((brand, i) => (
@@ -27,16 +26,15 @@ const SingleImage = ({ brand }) => {
   const { imageSrc, altText } = brand;
   return (
     <div>
-      <img src={imageSrc} alt={altText} className="h-8 w-16 md:h-14 md:w-24 lg:h-20 lg:w-40" />
+      <img
+        src={imageSrc}
+        alt={altText}
+        className="h-8 w-16 md:h-14 md:w-24 lg:h-20 lg:w-40"
+      />
     </div>
   );
 };
 
-
 SingleImage.propTypes = {
   brand: PropTypes.object,
 };
-
-
-
-
