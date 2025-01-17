63e5a0f9b1dbe6f87aa383b1fef7c8af4580e880
Abdul hameed <abdulhameed000650@gmail.com>
2025-01-18 02:58:27 +0500

    uodates

--- DevHoks/src/Pages/Home/HomeComponents/Satatistics/StatsCard.jsx
+++ DevHoks/src/Pages/Home/HomeComponents/Satatistics/StatsCard.jsx
@@ -4,9 +4,9 @@ import PropTypes from "prop-types";
 const StatsCard = ({ prop }) => {
   return (
     <div className="flex flex-row justify-center p-5 rounded-lg border border-green-700 gap-4">
-      <div className="hidden md:flex items-center justify-center bg-green-200 text-green-700 rounded-full w-16 h-16">
+      <div className="hidden md:flex items-center justify-center bg-green-200 text-green-700 rounded-full w-12 h-12">
         <i
-          className={`${prop.icon} text-4xl text-green-700 bg-green-200 p-3 rounded-full`}
+          className={`${prop.icon} text-3xl text-green-700 bg-green-200 p-3 rounded-full`}
         ></i>
       </div>
       <div className="leading-relaxed tracking-wide text-gray-700 flex flex-col gap-4">
